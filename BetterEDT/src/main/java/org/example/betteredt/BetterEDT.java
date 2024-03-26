7e9f5601149caf48812926668b8687fff7387b94
JulesA <jules.arsac@alumni.univ-avignon.fr>
2024-03-26 17:12:32 +0100

    🔥Hotfix: removed default user (my bad)🔥

--- BetterEDT/src/main/java/org/example/betteredt/BetterEDT.java
+++ BetterEDT/src/main/java/org/example/betteredt/BetterEDT.java
@@ -39,7 +39,6 @@ public class BetterEDT extends Application {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        user = new User(1, "admin", true, false, 1);
         if (user == null) {
             FXMLLoader fxmlLoader = new FXMLLoader(BetterEDT.class.getResource("connectionScreen.fxml"));
             mainScene = new Scene(fxmlLoader.load(), 1000, 600);
